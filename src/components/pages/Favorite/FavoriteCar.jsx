9a2fc8d10198c0c8fb7beab461f7904d6f7b964c
Volodymyr Vitiuk <122730435+VitiukVV@users.noreply.github.com>
2024-02-04 00:26:50 +0200

    add favorite page

--- src/components/pages/Favorite/FavoriteCar.jsx
+++ src/components/pages/Favorite/FavoriteCar.jsx
@@ -1,10 +1,37 @@
-// сторінка з оголошеннями, які були додані користувачем в улюблені Зовнішній вигляд програми повинен складатися з навігації(оформлення на ваш розсуд) та області перегляду.
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectFavoriteAdverts } from '../../../redux/favorite/favorite-selectors';
+import CatalogList from '../../catalogList/CatalogList';
+import Button from '../../button/Button';
+import { useNavigate } from 'react-router-dom';
+import {
+  Description,
+  FavoritePageContainer,
+  PageTitle,
+} from './FavoriteCar.styled';
 
 const FavoriteCar = () => {
-  return (
-    <div>FavoriteCar</div>
-  )
-}
+  const navigate = useNavigate();
+  const handleCatalogButtonClick = () => {
+    navigate('/catalog');
+  };
 
-export default FavoriteCar
+  const favoriteCars = useSelector(selectFavoriteAdverts);
+
+  return favoriteCars.length > 0 ? (
+    <CatalogList adverts={favoriteCars} />
+  ) : (
+    <FavoritePageContainer>
+      <PageTitle>Your Favorite Cars</PageTitle>
+      <Description>
+        It looks like your favorite cars list is currently empty. Discover and
+        add cars that catch your eye to personalize your collection.
+      </Description>
+      <Button type="button" handlerClick={handleCatalogButtonClick}>
+        Go to Catalog
+      </Button>
+    </FavoritePageContainer>
+  );
+};
+
+export default FavoriteCar;
---
+++ src/components/pages/Favorite/FavoriteCar.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const FavoritePageContainer = styled.section`
+  margin: 32px;
+  padding: 40px;
+  box-shadow: 0 0 13px 3px grey;
+  text-align: center;
+`;
+
+export const PageTitle = styled.h1`
+  font-size: 24px;
+  color: #333;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const Description = styled.p`
+  font-size: 1.2em;
+  line-height: 1.6;
+  margin-bottom: 20px;
+`;
